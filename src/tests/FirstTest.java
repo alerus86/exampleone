24f5b36934878f19fc33c1b4312ea2ebac552b69
Alejandro Ruske <aruske@overactiveinc.com>
2019-06-04 18:06:47 -0300

    First staging commit

--- src/tests/FirstTest.java
+++ src/tests/FirstTest.java
@@ -1,31 +1,68 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.Test;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.*;
+
+import static base.Settings.*;
 
 
 public class FirstTest {
 
+    @FindBy(id = "email")
+    public WebElement inputEmail;
+
+    @FindBy (id = "pass")
+    public WebElement inputPass;
+
+    @FindBy (xpath  = "//input[@value='Log In']")
+    public WebElement buttonLogIn;
+
     private WebDriver driver = null;
     private ChromeOptions chromeOptions = null;
 
-    @Test
-    public void firsExampleTest()
-    {
+    @BeforeSuite
 
-        System.setProperty("webdriver.chrome.driver" , "C:\\Program Files\\chromedriver.exe");
-
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver" , RESOURCES + CHROMEDRIVER);
         chromeOptions = new ChromeOptions();
-
         driver = new ChromeDriver(chromeOptions);
 
-        driver.navigate().to("http://www.google.com");
+    }
 
-
-
+    @AfterSuite
+    public void tearDown(){
         driver.quit();
+    }
+
+    @BeforeMethod
+    public void navigateToBaseUrl(){
+        driver.navigate().to(BASE_URL);
+    }
+
+    @Test
+    public void firstExampleTest() throws InterruptedException {
+
+        driver.manage().window().maximize();
+        driver.findElement(By.id("email")).sendKeys("test@test.com");
+        driver.findElement(By.id("pass")).sendKeys("test");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+        Thread.sleep(3000);
+
+    }
+
+    @Test
+    public void secondExampleTest() throws InterruptedException {
+
+        driver.manage().window().maximize();
+        driver.findElement(By.id("email")).sendKeys("test@test.com");
+        driver.findElement(By.id("pass")).sendKeys("test");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+        Thread.sleep(3000);
 
     }
 
